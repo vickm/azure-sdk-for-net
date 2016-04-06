e7d9a08c358b20a1dfea01f145efff50636a7ce0
Hervey Wilson <herveyw@microsoft.com>
2016-04-05 17:11:18 -0700

    Bump dependency on Extensions.

--- src/KeyVault/Microsoft.Azure.KeyVault.Jose/Properties/AssemblyInfo.cs
+++ src/KeyVault/Microsoft.Azure.KeyVault.Jose/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Resources;
 [assembly: AssemblyDescription( "Provides extended capabilities for Azure Key Vault." )]
 
 [assembly: AssemblyVersion("0.9.0.0")]
-[assembly: AssemblyFileVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.1.0")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("Azure .NET SDK")]
